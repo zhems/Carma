5f308ab7b46f33812afc5c3660478d3454e968a7
kyuyjay <qyongjian@gmail.com>
2019-12-02 01:37:29 -0500

    Frontend now fully functional

--- www/app.js
+++ www/app.js
@@ -16,23 +16,43 @@ app.use('/', function(req,res,next) {
 
 app.use('/', express.static('static'));
 
-app.use('/data/:zip/:genre',function(req,res,next) {
+app.use('/data/:zip/',function(req,res,next) {
     mgd.connect(function() {
         console.log("Connected to local db");
         const db = mgd.db("carma");
-        var cursor;
-        console.log(req.params.genre);
-        if (req.params.genre == "offroad") {
-            cursor = db.collection('test').find({"build.make" : "Nissan"})
-        } else {
-            cursor = db.collection('test').find()
-        }
-        data = cursor.toArray();
-        data.then(function(data) {
-            payload = {"listings": data};
-            res.json(payload);
+        console.log("Looking for listings near " + req.params.zip);
+        var latlong = db.collection("zips").find({Zip:parseInt(req.params.zip)}).toArray()
+        var y_dist
+        var x_dist
+        latlong.then(function(latlong) {
+            try {
+                var lat = latlong[0].Latitude;
+                var long_ = latlong[0].Longitude;
+                y_dist = [parseFloat(lat) - 0.5, parseFloat(lat) + 0.5];
+                x_dist = [parseFloat(long_) - 0.5, parseFloat(long_) + 0.5];
+                var cursor;
+                cursor = db.collection('prod').find({$and: [{lat: {$gt: y_dist[0]}}, 
+                                                    {lat: {$lt: y_dist[1]}},
+                                                    {long: {$gt: x_dist[0]}},
+                                                    {long: {$lt: x_dist[1]}}
+                                                ]}).limit(100)
+                var data = cursor.toArray();
+                data.then(function(data) {
+                    payload = {"listings": data};
+                    res.json(payload);
+                }, function(err) {
+                    console.log("Error " + err)
+                });
+            }
+            catch(error) {
+                console.log("Zip not found")
+                payload = {"listings" : []};
+                res.json(payload);
+            }
         }, function(err) {
-            console.log("Error " + err)
+            console.log("Zip not found")
+            payload = {"listings" : []};
+            res.json(payload);
         });
     });
 });
--- www/static/src/js/index.js
+++ www/static/src/js/index.js
@@ -26,14 +26,36 @@ payload.open("POST","https://api.cars.com/InventorySearchService/1.0/rest/invent
 
 d3.select("#landing-banner").attr("width",document.getElementById("landing-bg").clientWidth);
 
-//System dimensions
-var title_height = document.getElementById("controls").clientHeight;
+var svg;
+var sim;
+
+var listings = [];
+var genres = [["all","All Cars"],["Genre Off Road","Off-Road"],["Genre Family","Family Friendly"],["Genre Fun","Performance"],["Genre Luxury","Luxury"]];
+var genre;
+
+d3.select("#micro-genres")
+    .selectAll("option")
+    .data(genres)
+    .enter()
+    .append("option")
+    .attr("value",function(d) {
+        return d[0]
+    })
+    .text(d => d[1]);
 
 //Tooltip for each listing
 var tip = d3.tip()
-    .direction("n")
-    .html(function(d,width) {
-        if (d.media.photo_links.length == 0) {
+    .direction(function(d) {
+        var title_height = document.getElementById("controls").clientHeight;
+        var height = window.innerHeight - title_height;
+        if (d.y < height / 2) {
+            return "s";
+        } else {
+            return "n";
+        }
+    })
+    .html(function(d,width,height) {
+        if (d.image_url.length == 0) {
             return "<div class=\"tip alert alert-dark\">" +
                 "<h4>" + d.build.year + " " + d.build.make + " " + d.build.model + "</h4>" +
                 "<br /><br />" +
@@ -42,37 +64,32 @@ var tip = d3.tip()
         } else {
             var pic_width = Math.min(300,(d3.select("#canvas").attr("width") - 20));
             return "<div class=\"tip alert alert-dark\">" +
-                "<h4>" + d.build.year + " " + d.build.make + " " + d.build.model + "</h4>" +
-                "<span>Price: " + d.price + "</span>" + 
+                "<h4>" + d.Year + " " + d.Make + " " + d.Model + "</h4>" +
+//                "<span>Price: " + d.price + "</span>" + 
                 "<br /><br />" +
                 "<img " + 
                 "width=" + pic_width + " " +
-                "src=" + d.media.photo_links[0] + " />" +
+                "src=" + d.image_url + " />" +
                 "</div>"
         }
     });
 
-var svg;
-var sim;
 
-var listings = [];
-var genres = [["all","All Cars"],["offroad","Off-road"],["family","Family Friendly"]];
+$('#filter-zip').keyup(function(e) {
+    var zip = document.getElementById("filter-zip").value;
+    if (zip.length == 5) {
+        document.getElementById("filter-zip").value = "";
+        d3.json("data/" + zip).then(function(dump) {
+            listings = dump.listings;
+            nav(listings);
+            rank();
+        });
+    }
+});
 
-//$("#side-toggler").on("click", function() {
-//    console.log("toggle");
-//    $("#filter-bar").toggleClass("active");
-//});
-
-$('#filter-nav').change(function() {
-    var zip = 30318
-    var genre = document.getElementById("filter-genres").value;
-
-    d3.json("data/" + zip + "/" + genre).then(function(dump) {
-        console.log(dump)
-        listings = dump.listings;
-        nav(listings);
-        filter(listings);
-    });
+$('#genres').change(function() {
+    genre = document.getElementById("filter-genres").value;
+    filter();
 });
 
 $('#filter').change(function() {
@@ -86,20 +103,22 @@ $('#rank').change(function() {
 $(document).on("click", "#side-toggler", function() { 
 });
 
-$(document).on("click", "#search", function() { 
+$(document).on("click", "#search", function() {
+    search();
+});
 
+$(document).on("submit", "#search-form", function(e) {
+    console.log(e)
+    if (e.which == 13) {
+        e.preventDefault();
+        search();
+    }
+});
+
+function search() {
+    var title_height = document.getElementById("controls").clientHeight;
     var height = window.innerHeight - title_height;
     var width = document.getElementById("testbed").clientWidth;
-
-    //Clustering positions
-    var collisionForce = d3.forceCollide(40)
-        .iterations(50);
-    var centeringForce = d3.forceCenter(width / 2,height / 2);
-
-    sim = d3.forceSimulation()
-            .force("collide",collisionForce)
-            .force("center",centeringForce)
-            .on("tick",ticked);
 
     //Main Canvas
     svg = d3.select("#testbed")
@@ -108,61 +127,39 @@ $(document).on("click", "#search", function() {
         .attr("width",width)
         .attr("height",height)
         .call(function(d) {
-            tip(d,width);
+            tip(d,width,height);
         })
         .call(function(d) {
             resize(d,sim)
         });
 
     var zip = document.getElementById("zip").value;
-    var genre = document.getElementById("micro-genres").value;
-    zip = 30318
+    if (zip == "") {
+        zip = 30318;
+    }
+    genre = document.getElementById("micro-genres").value;
 
     //Sample dataset for development purposes
-    d3.json("data/" + zip + "/" + genre).then(function(dump) {
-        console.log(dump)
+    d3.json("data/" + zip).then(function(dump) {
         listings = dump.listings;
         nav(listings);
-        filter(listings);
+        rank();
     });
-});
-
-
-//Do NOT use API.
-//It works but we only have 300 calls per MONTH
-/***********************************************************
-
-var payload = new XMLHttpRequest();
-payload.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-
-        var listings = JSON.parse(this.responseText).listings;
-        draw(listings);
-    }
 };
-
-var key = "oG1gTKvIs4P8WUgpEzFuNRh8GVYD8z0B";
-
-payload.open("GET","http://marketcheck-prod.apigee.net/v1/search?api_key=" + key + "&latitude=33.75&longitude=-84.39&radius=100&car_type=used&start=0&rows=50");
-payload.setRequestHeader("Content-Type","application/json");
-payload.send();
-
-************************************************************/
 
 //Dynamically build filter list
 function nav(listings) {
-
+    
     d3.select("#filter-genres")
         .selectAll("option")
         .data(genres)
         .enter()
         .append("option")
         .attr("value",function(d) {
-            console.log(d)
             return d[0]
         })
         .text(d => d[1]);
-
+/*
     var price = d3.extent(listings,function(d) {
         return d.price;
     });
@@ -174,9 +171,9 @@ function nav(listings) {
                         .attr("min",price[0])
                         .attr("max",price[1])
                         .attr("value",price[1]);
-
+*/
     var year = d3.extent(listings,function(d) {
-        return d.build.year;
+        return d.Year;
     });
 
     d3.select("#filter-year-label")
@@ -193,7 +190,7 @@ function nav(listings) {
         .exit().remove();
 
     var make = d3.set(listings,function(d) {
-        return d.build.make;
+        return d.Make;
     }).values().sort();
     make.unshift("All Brands");
 
@@ -216,7 +213,7 @@ function nav(listings) {
         .exit().remove();
 
     var style = d3.set(listings,function(d) {
-        return d.build.body_type;
+        return d.bodystyles;
     }).values().filter(x => x != "undefined").sort();
     style.unshift("All Body Styles");
 
@@ -238,22 +235,54 @@ function nav(listings) {
 //Draw nodes
 function draw(f_listings) {
 
-    var temp = d3.extent(f_listings,function(d) {
-        return d.build.year;
-    });   
+    var title_height = document.getElementById("controls").clientHeight;
+    var height = window.innerHeight - title_height;
+    var width = document.getElementById("testbed").clientWidth;
 
     //Circles
     svg.selectAll("text")
         .remove();
 
-    console.log(f_listings);
+    //Clustering positions
+    var collisionForce = d3.forceCollide(function(d) {
+            return d.r + 10;
+        })
+        .strength(1)
+        .iterations(200);
+    var centeringForce = d3.forceCenter(width / 2,height / 2)
+    var splitForce = d3.forceRadial()
+        .x(width / 2)
+        .y(height / 2)
+        .radius(function(d) {
+            if (d[genre] != "True") {
+                return 300;
+            } else {
+                return 0;
+            }
+        })
+        .strength(0.4);
+    var chargeForce = d3.forceManyBody()
+        .strength(-60)
+        .distanceMax(function(d) {
+            return d.r + 10
+        });
 
-    sim.nodes(f_listings);
-    sim.force("collide").radius(function(d) {
-        return (((temp[1] - d.build.year) / (temp[1] - temp[0])) * 30) + 5
-    });
-    sim.tick(10);
+    sim = d3.forceSimulation(f_listings)
+            .force("collide",collisionForce)
+            //.force("center",centeringForce)
+            .force("charge",chargeForce)
+            .force("split",splitForce)
+            .on("tick",ticked);
+
     sim.stop();
+    sim.tick();
+    sim.tick();
+    sim.tick();
+    sim.tick();
+    sim.tick();
+    sim.tick();
+    sim.tick();
+    sim.tick();
 
     var circles = svg.selectAll("circle")
         .data(sim.nodes())
@@ -267,7 +296,7 @@ function draw(f_listings) {
     var nodes = circles.enter()
         .append("circle")
         .attr("r",function(d) {
-            return ((temp[1] - d.build.year) / (temp[1] - temp[0])) * 30;
+            return d.r;
         })
         .attr("cx",function(d) {
             return d.x;
@@ -276,26 +305,43 @@ function draw(f_listings) {
             return d.y;
         })
         .style("fill-opacity",0)
-        .classed("node",true)
+        .style("fill",function(d) {
+            if (d[genre] == "False") {
+                return "#E6E6E6";
+            } else {
+                return "#F94D4D";
+            }
+        })
         .on("mouseover",tip.show)
         .on("mouseout",tip.hide)
         .on("click",function(d) {
-            window.open(d.vdp_url);
+            window.open(d.url);
         })
         .transition()
             .duration(300)
-            .style("fill-opacity",1);
+            .style("fill-opacity",1)
+            .attr("r",function(d) {
+                return d.r;
+            });
 
     circles.transition()
-        .duration(300)
+        .duration(400)
         .attr("cx",function(d) {
             return d.x;
         })
         .attr("cy",function(d) {
             return d.y;
+        })
+        .attr("r",function(d) {
+            return d.r;
+        })
+        .style("fill",function(d) {
+            if (d[genre] == "False") {
+                return "#E6E6E6";
+            } else {
+                return "#F94D4D";
+            }
         });
-
-    sim.restart();
 
     if (f_listings.length == 0) {
         svg_height = svg.attr("height");
@@ -338,17 +384,17 @@ function resize(svg,force) {
 //Filter function
 function filter() {
     var filtered = listings;
-     
+/* 
     var max_price = document.getElementById("filter-price").value;
     filtered = filtered.filter(function(d) {
         return d.price <= max_price;
     });
     d3.select("#filter-price-label")
         .text("Maximum Price: " + max_price);
-
+*/
     var min_year = document.getElementById("filter-year").value;
     filtered = filtered.filter(function(d) {
-        return d.build.year >= min_year;
+        return d.Year >= min_year;
     });
     d3.select("#filter-year-label")
         .text("Minimum Year: " + min_year);
@@ -356,16 +402,41 @@ function filter() {
     var selected_make = document.getElementById("filter-make").value;
     if (selected_make != "All Brands") {
         filtered = filtered.filter(function(d) {
-            return d.build.make == selected_make;
+            return d.Make == selected_make;
         });
     } 
 
     var selected_style = document.getElementById("filter-style").value;
     if (selected_style != "All Body Styles") {
         filtered = filtered.filter(function(d) {
-            return d.build.body_type == selected_style;
+            return d.bodystyles == selected_style;
         });
     }
 
     draw(filtered);
+}
+
+function rank() {
+    var reliability = +(document.getElementById("rank-reliable").value) + 0.1;
+    var performance = +(document.getElementById("rank-performance").value)+ 0.1;
+    var mpg = +(document.getElementById("rank-mpg").value) + 0.1;
+    var resale = +(document.getElementById("rank-resale").value) + 0.1;
+
+    listings.forEach(function(d) {
+        d.rank = (reliability * +d["Reliability"]) +
+            (performance * +d["Driving Experience"]) +
+            (mpg * +d["Fuel Economy"]) +
+            (resale * +d["Resale Value"])
+    });
+    ranking = d3.extent(listings,function(d) {
+        return d.rank;
+    });
+    r_scale = d3.scaleSqrt()
+        .domain(ranking)
+        .range([2,35]);
+
+    listings.forEach(function(d) {
+        d.r = r_scale(d.rank);
+    });
+    filter();
 }
